e5fe60e2cd4902ba6e99b8a947060d87d342ef4c
constanxe <54532144+constanxe@users.noreply.github.com>
2020-05-08 16:34:44 +0800

    rename to challenge2 & add challenge1 folder

---
+++ challenge 1/magic.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    // animate
+    $(".swap").click(function(){
+        $(".container").toggleClass("sign-up");
+    });
+
+    // add password matching validation
+    // https://codepen.io/diegoleme/pen/surIK
+    var password = document.getElementById("Password")
+    , confirm_password = document.getElementById("ConfirmPassword");
+    function validatePassword(){
+        if(password.value != confirm_password.value) {
+            confirm_password.setCustomValidity("password does not match");
+        } else {
+            confirm_password.setCustomValidity('');
+        }
+    }
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+
+    // alert
+    $("form").submit(function(){
+        if ( $(this).hasClass("login") ) {
+            alert("login!");
+        } else {
+            alert("register!");
+        }
+        event.preventDefault();
+    });
+
+});
